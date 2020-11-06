bc273f9f1852d4c203d329757fccd0303dadba35
MohammadAlaklouk <61121384+MohammadAlaklouk@users.noreply.github.com>
2020-11-06 14:10:30 +0200

    Commit

---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+Button button;
+TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        button.setTextColor(Color.BLACK);
+        button.setBackgroundColor(Color.GREEN);
+        button.setText(R.string.my_name);
+      button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+
+              textView.setTextColor(Color.BLACK);
+              textView.setBackgroundColor(Color.GREEN);
+              textView.setText(R.string.clicked);
+
+          }
+      });
+    }
+
+
+
+
+
+/*
+    public void clickMybutton(View view){
+
+        textView.setTextColor(Color.BLACK);
+        textView.setBackgroundColor(Color.GREEN);
+        textView.setText(R.string.clicked);
+    }*/
+
+
+}
